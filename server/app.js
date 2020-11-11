597796c99dbfef541ab0b390d309ceddd6d41eb4
Thomas Stussi <tstussi06@gmail.com>
2020-11-11 11:24:03 -0800

    server recieves message from client

--- server/app.js
+++ server/app.js
@@ -1,18 +1,24 @@
 // server
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 
-server = app.listen(7890, function () {
-  console.log('server is running')
-})
+server = app.listen(7890, () => {
+  console.log('server is running');
+});
 
 // bring in socketio
-const socket = require('socket.io')
-io = socket(server)
+const socket = require('socket.io');
+io = socket(server);
 
 // socket connection 
 io.on('connection', (socket) => {
-  console.log(socket.id, 'SOCKET ID')
+  console.log(socket.id, 'SOCKET ID');
   // console.log(socket, 'SOCKET')
-})
+
+  //SEND_MESSAGE event received
+  socket.on('SEND_MESSAGE', (data) => {
+    io.emit('RECIEVE_MESSAGE', data);
+    console.log(data);
+  });
+});
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import socket io 
-import io from "socket.io-client";
+import io from 'socket.io-client';
 
 export default function App() {
+  const [userName, setUserName] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
 
   const socket = io('localhost:7890');
 
-  console.log('in app jsx')
+  console.log('in app jsx');
+
+  const handleMessage = ({ target }) => {
+    setMessage(target.value);
+  };
+  const handleUserName = ({ target }) => {
+    setUserName(target.value);
+  };
+
+  const handleClick = () => {
+    socket.emit('SEND_MESSAGE', {
+      author: userName,
+      message
+    });
+    setMessage('');
+  };
 
   return (
-    <h1>hi {JSON.stringify(socket)}</h1>
-  )
+    <div>
+      <input onChange={handleUserName} type="text" placeholder="Username" />
+      <br/>
+      <input onChange={handleMessage} type="text" placeholder="Message" />
+      <br/>
+      <button onClick={handleClick}>Send</button>
+    </div>
+  );
 }
